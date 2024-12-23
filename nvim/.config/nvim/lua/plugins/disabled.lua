5ad516bb292f8ba3ccba066975443d379018501e
Zenon 'SI <si.zenon@gmail.com>
2024-12-23 09:56:49 +0700

    feat(nvim): disable blink.cmp and re-enable nvim-cmp

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -4,7 +4,7 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "echasnovski/mini.surround", enabled = false },
-  -- { "hrsh7th/cmp-buffer", enabled = false },
-  -- { "hrsh7th/cmp-path", enabled = false },
+  { "hrsh7th/cmp-buffer", enabled = false },
+  { "hrsh7th/cmp-path", enabled = false },
   { "rafamadriz/friendly-snippets", enabled = false },
 }
