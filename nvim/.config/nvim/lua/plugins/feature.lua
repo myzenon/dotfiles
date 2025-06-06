bcebb9d201f3086609579a5ce4de67625eee5a9d
Zenon 'SI <si.zenon@gmail.com>
2025-06-06 19:30:11 +0700

    feat(nvim): try using new add/dec keyt

--- nvim/.config/nvim/lua/plugins/feature.lua
+++ nvim/.config/nvim/lua/plugins/feature.lua
@@ -42,6 +42,8 @@ return {
     keys = {
       { "+", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
       { "-", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
     },
     config = function()
       local augend = require("dial.augend")
